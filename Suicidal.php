562c44e6b454d0e9487563660acec41c7d3bae06
AplusComics <Apluscomics@live.com>
2014-01-05 19:46:56 -0500

    Create Suicidal.php

---
+++ Suicidal.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+__PocketMine Plugin__
+name=Suicidal
+version=1.0
+description=Allows people to commit suicide
+author=A+Comics
+class=Kill
+apiversion=10
+*/
+
+class Kill implements Plugin{
+
+private $api;
+
+public function __construct(ServerAPI $api, $server = false){
+$this->api = $api;
+}
+
+public function init(){
+$this->api->console->register("suicide","Commit suicide!",array($this, "Suicide"));
+$this->api->ban->cmdWhitelist("suicide");
+}
+
+public function Suicide($cmd, $issuer){
+$username = $issuer->username;
+$this->api->console->run("kill " . $username);
+}
+
+public function __destruct(){
+}
+}
+?>
